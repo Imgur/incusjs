08afa7cc8ba67e5ecc433c0317b1fee51ae02d89
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2015-08-06 17:08:05 -0700

    Add presence.js

---
+++ example/presence.js
@@ -0,0 +1,76 @@
+var INCUS_COMMAND_SETPRESENCE = 'setpresence';
+var MAXIMUM_COMMNANDS_PER_SECOND = 20;
+
+/**
+ * Notify Incus when we go from being inactive to active or vice versa.
+ */
+var Presence = function(incus, inactivityThresholdSeconds) {
+    this.incus = incus;
+    this.timerReference = null;
+    this.lastActiveTime = null;
+    this.inactivityThresholdSeconds = (typeof inactivityThresholdSeconds !== 'undefined' ? inactivityThresholdSeconds : 60);
+
+    document.body.addEventListener('touchstart', this.handleUserActivity.bind(this));
+    document.body.addEventListener('keypress', this.handleUserActivity.bind(this));
+    document.body.addEventListener('mousemove', this.handleUserActivity.bind(this));
+
+    this.reset();
+
+    this.notifyIncus({
+        present: true
+    });
+};
+
+Presence.prototype = {
+    reset: function() {
+        var nowUnixTime = (+ new Date / 1000);
+
+        if(this.lastActiveTime !== null) {
+            var wasPresent = (this.lastActiveTime + this.inactivityThresholdSeconds >= nowUnixTime);
+
+            if(!wasPresent) {
+                this.notifyIncus({
+                    present: true
+                });
+            }
+        }
+
+        this.lastActiveTime = nowUnixTime;
+
+        if(this.timerReference !== null) {
+            window.clearTimeout(this.timerReference);
+        }
+
+        this.timerReference = window.setTimeout(this.handleTimeout.bind(this), this.inactivityThresholdSeconds * 1000);
+    },
+
+    notifyIncus: function(options) {
+        var command = {
+            'command': INCUS_COMMAND_SETPRESENCE
+        };
+
+        var message = {
+            'present': options.present
+        };
+
+        this.incus.send(this.incus.newCommand(command, message));
+    },
+
+    handleTimeout: function() {
+        this.notifyIncus({
+            present: false
+        });
+    },
+
+    handleUserActivity: function(e) {
+        var nowUnixTimeMsec = (+ new Date);
+
+        // Throttle reset commands
+        if(this.handleUserActivity.lastSuccessfulCall === null || this.handleUserActivity.lastSuccessfulCall + (1000 / MAXIMUM_COMMNANDS_PER_SECOND) <= nowUnixTimeMsec) {
+            this.handleUserActivity.lastSuccessfulCall = nowUnixTimeMsec;
+            this.reset();
+        }
+    },
+};
+
+module.exports = Presence;
